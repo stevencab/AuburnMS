8b018fc6df3f544c60ceac68b97bab163de62f67
Steven Cabrera <smc0124@auburn.edu>
2022-09-12 19:59:51 -0500

    beginning midstory analysis

---
+++ r_scripts/midstory removal/main midstory analysis.R
@@ -0,0 +1,14 @@
+# scripts for beginning midstory removal analysis
+#things to do
+#all leaf litter sorted analysis, box plots for each collection 
+#sort leaf litter for comp at time of fire
+#tree overstory composition
+#burn data differences between season, os treat, and thinning
+#interaction between all 3
+
+library(ggplot2)
+library(readr)
+library(dplyr)
+library(cowplot)
+library(ggsignif)
+
